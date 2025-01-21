ba7300eb73ccd801a06b84d64f6f2c8ee09c0def
abhijit732023 <abhijitgautam2004@gmail.com>
2025-01-21 21:35:38 +0530

    envChanged1

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,12 +1,12 @@
 const conf = {
-    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    appwriteProfilecCollectionID: String(import.meta.env.VITE_APPWRITE_PROFILE_COLLECTION_ID),
-    appwriteProfilecBuketID: String(import.meta.env.VITE_APPWRITE_PROFILE_BUCKET_ID),
-    appwriteCommentCollectionID:String(import.meta.env.VITE_APPWRITE_COMMENT_COLLECTION_ID)
+    appwriteUrl: String(import.meta.env.VITE_API_URL),
+    appwriteProjectId: String(import.meta.env.VITE_API_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_API_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_API_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_API_BUCKET_ID),
+    appwriteProfilecCollectionID: String(import.meta.env.VITE_API_PROFILE_COLLECTION_ID),
+    appwriteProfilecBuketID: String(import.meta.env.VITE_API_PROFILE_BUCKET_ID),
+    appwriteCommentCollectionID:String(import.meta.env.VITE_API_COMMENT_COLLECTION_ID)
 }
 // there was a name issue with the import.meta.env.VITE_APPWRITE_URL, it was later fixed in debugging video
 
